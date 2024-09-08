db21380d4a292641f6c2c03cd360d12858aefa9d
Natalya Efimova <natkaef.1996@mail.ru>
2024-09-08 13:19:09 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const cardTemplate = document.querySelector('#card-template').content;
+
+const placesList = document.querySelector('.places__list');
+
+function createCards(item) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = card.querySelector('.card__delete-button');
+
+  const cardImage = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+
+  cardImage.src = item.link;
+  cardTitle.textContent = item.name;
+  
+  deleteButton.addEventListener('click',  deleteCards);
+
+  return card;
+};
+
+function deleteCards(event) {
+  const card = event.target.closest('.card');
+  card.remove();
+}
+
+initialCards.forEach(function(item) {
+  const newCard = createCards(item, deleteCards);
+  placesList.append(newCard);
+});
